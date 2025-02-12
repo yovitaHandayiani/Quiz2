485d15dccf0b4c96f89c5301c34eb5bf1163a25c
yovitaHandayiani <116541773+yovitaHandayiani@users.noreply.github.com>
2025-02-12 16:10:20 +0700

    Make Modul

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Quiz2"
 include(":app")
- 
+include(":uikit")
--- uikit/build.gradle.kts
+++ uikit/build.gradle.kts
@@ -1,21 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-parcelize")
 }
 
 android {
-    namespace = "quiz.navigation"
+    namespace = "jd.co.edts.training.uikit"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "quiz.navigation"
         minSdk = 30
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -34,9 +30,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures{
-        viewBinding = true
-    }
 }
 
 dependencies {
@@ -44,12 +37,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation("com.google.android.material:material:1.6.0") // For TabLayout and ViewPager2
-    implementation("androidx.viewpager2:viewpager2:1.0.0")  // For ViewPager2
-    implementation(project(":uikit"))
 }
